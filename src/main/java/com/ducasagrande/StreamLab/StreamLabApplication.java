c95dcc129b632cda142b74db30928232503c1120
Eduardo <eduardocasagrande71@gmail.com>
2025-05-16 22:54:12 -0300

    Initial Commit

---
+++ src/main/java/com/ducasagrande/StreamLab/StreamLabApplication.java
@@ -0,0 +1,13 @@
+package com.ducasagrande.StreamLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ducasagrande/StreamLab/StreamLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducasagrande.StreamLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
